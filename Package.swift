e8413146e1706180c72b1a686b4b8c4dbf2bb085
Wesley Hilhorst <wesley.hilhorst@justpinch.com>
2024-05-31 19:47:09 +0200

    Create version 4.7.1

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 struct PackageMetadata {
-	static let version = "4.8.1"
+	static let version = "4.7.1"
 	static let checksum = "368f28a743383b3cad9d7188e7af5c1e6f5e9995d24022a0226218e73d1278c3"
 }
 
